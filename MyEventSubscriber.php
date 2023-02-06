afafea3c7f6378dcc1f6718e6f24f68dbee9e6dc
shahzaib <shahzaib@nitropack.com>
2023-02-06 16:38:06 +0500

    Composer Testing

--- MyEventSubscriber.php
+++ MyEventSubscriber.php
@@ -2,16 +2,13 @@
 
 namespace NitroPack\NitroPack;
 
-use Composer\Composer;
 use Composer\EventDispatcher\EventSubscriberInterface;
-use Composer\IO\IOInterface;
 use Composer\Plugin\PluginEvents;
-use Composer\Plugin\PluginInterface;
-use Composer\Script\Event;
+
 use Composer\Plugin\PrePoolCreateEvent;
 
 
-class MyEventSubscriber implements EventSubscriberInterface, PluginInterface
+class MyEventSubscriber implements EventSubscriberInterface
 {
 
     protected $composer;
@@ -20,7 +17,7 @@ class MyEventSubscriber implements EventSubscriberInterface, PluginInterface
     public static function getSubscribedEvents()
     {
         return [
-            'post-package-install' => ['onPostPackageInstall', 1000],
+            'pre-package-uninstall'=> ['onPrePoolCreate', 1000],
             PluginEvents::PRE_POOL_CREATE => ['onPrePoolCreate', 1000],
         ];
     }
@@ -30,28 +27,5 @@ class MyEventSubscriber implements EventSubscriberInterface, PluginInterface
         die("HELLO");
     }
 
-    public function onPostPackageInstall(Event $event)
-    {
-        $io = $event->getIO();
-        $io->write('Running custom script after package install');
-    }
 
-
-    public function activate(Composer $composer, IOInterface $io)
-    {
-        $this->composer = $composer;
-        $this->io = $io;
-        $installer = new TemplateInstaller($io, $composer);
-        $composer->getInstallationManager()->addInstaller($installer);
-    }
-
-    public function uninstall(Composer $composer, IOInterface $io)
-    {
-        // TODO: Implement uninstall() method.
-    }
-
-    public function deactivate(Composer $composer, IOInterface $io)
-    {
-        // TODO: Implement deactivate() method.
-    }
 }
