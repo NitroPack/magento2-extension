a64a6f1698fb65dd88794ac63b7d9f4cc918ed6e
shahzaib <shahzaib@nitropack.com>
2023-02-06 17:51:22 +0500

    Uninstall Event trigger By composer plugin

--- ComposerUninstall.php
+++ ComposerUninstall.php
@@ -3,53 +3,67 @@
 namespace NitroPack\NitroPack;
 
 use Composer\Composer;
-use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
-use Composer\Plugin\PluginEvents;
-
 use Composer\Plugin\PluginInterface;
-use Composer\Plugin\PrePoolCreateEvent;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\App\Config\Storage\WriterInterface;
-
+use NitroPack\NitroPack\Helper\VarnishHelper;
 
 class ComposerUninstall implements PluginInterface
 {
 
+    /**
+     * @var \NitroPack\NitroPack\Model\NitroPackEvent\Trigger
+     * */
+    protected $trigger;
+    /**
+     * @var WriterInterface
+     * */
+    protected $configWriter;
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     * */
+    protected $scopeConfig;
+    /**
+     * @var \NitroPack\NitroPack\Helper\VarnishHelper
+     * */
+    protected $varnishHelper;
+
+    public function __construct(
+        \NitroPack\NitroPack\Model\NitroPackEvent\Trigger $trigger,
+        WriterInterface $configWriter,
+        \NitroPack\NitroPack\Helper\VarnishHelper $varnishHelper,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+    ) {
+        $this->trigger = $trigger;
+        $this->configWriter = $configWriter;
+        $this->varnishHelper = $varnishHelper;
+        $this->scopeConfig = $scopeConfig;
+    }
+
     public function uninstall(Composer $composer, IOInterface $io)
     {
-        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        $trigger = $objectManager->create(\NitroPack\NitroPack\Model\NitroPackEvent\Trigger::class);
-        $scopeConfig = $objectManager->create(\Magento\Framework\App\Config\ScopeConfigInterface::class);
-        $configWriter = $objectManager->create(WriterInterface::class);
-        $trigger->hitEvent('uninstall', false);
-
-        if ($scopeConfig->getValue('system/full_page_cache/varnish_enable')) {
-            $configWriter->save(
-                'system/full_page_cache/caching_application',
-                \Magento\PageCache\Model\Config::VARNISH,
-                $scope = ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
-                $scopeId = 0
-            );
+        //TRIGGER AN EVENT OF UNINSTALL
+        $this->trigger->hitEvent('uninstall', false);
+        if ($this->scopeConfig->getValue('system/full_page_cache/varnish_enable')) {
+            $this->setData('system/full_page_cache/caching_application', \Magento\PageCache\Model\Config::VARNISH);
         } else {
-            $configWriter->save(
-                'system/full_page_cache/caching_application',
-                \Magento\PageCache\Model\Config::BUILT_IN,
-                $scope = ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
-                $scopeId = 0
-            );
+            $this->setData('system/full_page_cache/caching_application', \Magento\PageCache\Model\Config::BUILT_IN);
         }
-        $helper = $objectManager->create(\NitroPack\NitroPack\Model\NitroPackEvent\Trigger::class);
-        $helper->purgeVarnish();
-
     }
 
     public function deactivate(Composer $composer, IOInterface $io)
     {
         // TODO: Implement deactivate() method.
     }
+
     public function activate(Composer $composer, IOInterface $io)
     {
         // TODO: Implement activate() method.
+    }
+
+    private function setData($path, $value)
+    {
+        $this->configWriter->save($path, $value, $scope = ScopeConfigInterface::SCOPE_TYPE_DEFAULT, $scopeId = 0);
     }
 }
