a8c66526b7e00747df38d33b69722dd3e6a5d20e
Shahzaib <shahzaib@nitropack.com>
2023-03-27 16:11:48 +0500

    Fix the cache flush for magento admin

--- Observer/FullPageCache/Clear.php
+++ Observer/FullPageCache/Clear.php
@@ -6,59 +6,117 @@ use Magento\Framework\App\Cache\StateInterface;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Filesystem\DirectoryList;
 use NitroPack\NitroPack\Api\NitroService;
 use NitroPack\NitroPack\Api\NitroServiceInterface;
+use NitroPack\NitroPack\Helper\ApiHelper;
+use NitroPack\NitroPack\Helper\RedisHelper;
+use NitroPack\NitroPack\Helper\VarnishHelper;
 use NitroPack\SDK\NitroPack;
 
 class Clear implements ObserverInterface
 {
     /**
-     * @var NitroServiceInterface
+     * @var DirectoryList
      * */
-    private $nitro;
+    protected $directoryList;
 
-    private $stores = null;
+    protected $settings = null;
+    protected $sdk = null;
+    /**
+     * @var ApiHelper
+     * */
+    protected $apiHelper;
+    /**
+     * @var RedisHelper
+     * */
+    protected $redisHelper;
     /**
      * @var ScopeConfigInterface
      * */
     protected $_scopeConfig;
     /**
-     * @var StateInterface
+     * @var VarnishHelper
      * */
-    protected $_cacheState;
-
+    protected $varnishHelper;
+    /**
+     * @var StateInterface;
+     * */
+    private   $_cacheState;
+    /**
+     * @param DirectoryList $directoryList
+     * @param ApiHelper $apiHelper
+     * @param ScopeConfigInterface $scopeConfig
+     * @param VarnishHelper $varnishHelper
+     * @param  StateInterface $_cacheState
+     * */
     public function __construct(
-        NitroServiceInterface $nitro,
-        \Magento\Store\Api\GroupRepositoryInterface $storeGroupRepository,
-        ScopeConfigInterface $_scopeConfig,
+        DirectoryList $directoryList,
+        ApiHelper $apiHelper,
+        VarnishHelper $varnishHelper,
+        ScopeConfigInterface $scopeConfig,
         StateInterface $_cacheState
+
     ) {
-        $this->nitro = $nitro;
-        $this->_scopeConfig = $_scopeConfig;
+        $this->apiHelper = $apiHelper;
+        $this->varnishHelper = $varnishHelper;
+        $this->_scopeConfig = $scopeConfig;
+        $this->directoryList = $directoryList;
         $this->_cacheState = $_cacheState;
-        /**
-         * @var \Magento\Store\Api\GroupRepositoryInterface $storeGroupRepository
-         */
-        $this->stores = $storeGroupRepository->getList();
     }
 
     public function execute(Observer $observer)
     {
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $storeRepo = $objectManager->create(\Magento\Store\Api\GroupRepositoryInterface::class);
+        $storeGroup = $storeRepo->getList();
         if (!is_null(
                 $this->_scopeConfig->getValue('system/full_page_cache/caching_application')
             ) && $this->_scopeConfig->getValue(
                 'system/full_page_cache/caching_application'
             ) == NitroService::FULL_PAGE_CACHE_NITROPACK_VALUE && $this->_cacheState->isEnabled('full_page')) {
-            foreach ($this->stores as $storeId => $storeData) {
-                if ($storeData->getCode() != 'admin') {
-                    $this->nitro->reload($storeData->getCode());
-                    if ($this->nitro->isConnected() && $this->nitro->isEnabled()) {
-                        $this->nitro->purgeCache(
-                            null,
-                            null,
-                            \NitroPack\SDK\PurgeType::COMPLETE,
-                            "Magento cache flush remove all page cache"
+
+            foreach ($storeGroup as $storesData) {
+                $settingsFilename = $this->apiHelper->getSettingsFilename($storesData->getCode());
+                $haveData = $this->apiHelper->readFile($settingsFilename);
+                if ($haveData) {
+                    $this->settings = json_decode($haveData);
+
+                    $rootPath = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR;
+                    try {
+                        $rootPath = $this->directoryList->getPath('var') . DIRECTORY_SEPARATOR;
+                    } catch (\Magento\Framework\Exception\FileSystemException $e) {
+                        // fallback to using the module directory
+                    }
+
+                    $cachePath = $rootPath . 'nitro_cache' . DIRECTORY_SEPARATOR . $this->settings->siteId;
+                    try {
+                        $this->sdk = new NitroPack(
+                            $this->settings->siteId, $this->settings->siteSecret, null, null, $cachePath
                         );
+                        if ($this->settings->enabled) {
+                            $this->sdk->purgeCache(
+                                null,
+                                null,
+                                \NitroPack\SDK\PurgeType::COMPLETE,
+                                "Magento cache flush remove all page cache"
+                            );
+                            if (
+                                !is_null($this->_scopeConfig->getValue(NitroService::FULL_PAGE_CACHE_NITROPACK))
+                                && $this->_scopeConfig->getValue(
+                                    NitroService::FULL_PAGE_CACHE_NITROPACK
+                                ) == NitroService::FULL_PAGE_CACHE_NITROPACK_VALUE
+
+                            ) {
+                                $this->varnishHelper->purgeVarnish();
+                            }
+                        }
+                    } catch (\Exception $e) {
+                        $file = $objectManager->create('\Magento\Framework\Filesystem\Driver\File');
+                        $cachePath = $rootPath . 'nitro_cache' . DIRECTORY_SEPARATOR . $this->settings->siteId;
+                        if ($file->isDirectory($cachePath)) {
+                            $file->deleteDirectory($cachePath);
+                        }
                     }
                 }
             }
